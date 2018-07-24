c2e5415f57e1cf0321ecb027cf4facd129accf45
Dopam1ne <roywhoa@gmailcom>
2018-07-24 01:06:31 -0700

    changed process.env.MONGODC_URI to ..MONGODB_URI

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 //connect to MongoDB named nytreact
-mg.connect(process.env.MONGODC_URI ||"mongodb://localhost/nytreact");
+mg.connect(process.env.MONGODB_URI ||"mongodb://localhost/nytreact");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
